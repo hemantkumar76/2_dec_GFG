769d0daf0a6b751ede86522c27d5992d9b029459
Harshvardhan <93473274+harshvardhan8058@users.noreply.github.com>
2023-10-18 16:06:07 +0530

    Update 2_december_gfg.cpp

--- 2_december_gfg.cpp
+++ 2_december_gfg.cpp
@@ -8,7 +8,48 @@ public:
     int isItPossible(string S, string T, int M, int N)
   {
     // write code here 
-    
+    int i = 0, j = 0;
+        while (i < M && j < N)
+        {
+            if (S[i] == '#')
+            {
+                i++;
+            }
+            else if (T[j] == '#')
+            {
+                j++;
+            }
+            else if (S[i] == 'A' && T[j] == 'B')
+            {
+                return 0;
+            }
+            else if (S[i] == 'B' && T[j] == 'A')
+            {
+                return 0;
+            }
+            else
+            {
+                i++;
+                j++;
+            }
+        }
+        while (i < M)
+        {
+            if (S[i] != '#')
+            {
+                return 0;
+            }
+            i++;
+        }
+        while (j < N)
+        {
+            if (T[j] != '#')
+            {
+                return 0;
+            }
+            j++;
+        }
+        return 1;
   }
 };
 int main()
